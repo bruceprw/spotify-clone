918c8445df7483c1445495b4048097a38271a90a
Bruce Williams <brucewilliams@roku.com>
2023-09-18 16:17:59 +0100

    Add useUser hook and supabase provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import Sidebar from '../components/Sidebar'
-
+import SupabaseProvider from '@/providers/SupabaseProvider'
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,10 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
-        </body>
+        <SupabaseProvider>
+          <Sidebar>
+            {children}
+          </Sidebar>
+        </SupabaseProvider>
+      </body>
     </html>
   )
 }
