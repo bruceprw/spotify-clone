42c8125bc85712ff00da4d3a9bc24dcf3e09d854
Bruce Williams <brucewilliams@roku.com>
2023-09-19 15:24:39 +0100

    Added song upload functionality

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -32,7 +32,7 @@ const AuthModal = () => {
             title="Welcome Back"
             description="Login to your account"
             isOpen={isOpen}
-            onChange={() => {}}
+            onChange={onChange}
         >
         <Auth
             theme="dark"
