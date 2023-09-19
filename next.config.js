b9191c7c6c44892b6bece59831ba5f2b59192732
Bruce Williams <brucewilliams@roku.com>
2023-09-19 16:58:28 +0100

    Add latest song list

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "fvpymcqnstorqywkdhmq.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
